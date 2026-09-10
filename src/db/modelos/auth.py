6721c14e6f628108ad8ebb6092640b352d580e48
Juan Cruz <juancruzgenovese22@gmail.com>
2026-09-10 13:58:54 -0300

    Un usuario siempre depende de una cuenta

--- src/db/modelos/auth.py
+++ src/db/modelos/auth.py
@@ -9,10 +9,6 @@ class CuentaAuth(Base, TimestampMixin):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     dni = Column(String(50), unique=True, nullable=False, index=True)
     password_hash = Column(String(255), nullable=False)
-    tipo_usuario = Column(String(25), nullable=False)
-    usuario_adm_id = Column(Integer, ForeignKey("USUARIOS_ADM.id", ondelete="CASCADE"), nullable=True)
-    usuario_cli_id = Column(Integer, ForeignKey("USUARIOS_CLI.id", ondelete="CASCADE"), nullable=True)
     activo = Column(Boolean, default=True, nullable=False)
 
-    usuario_adm = relationship("UsuarioAdm", backref="cuenta_auth", uselist=False)
-    usuario_cli = relationship("UsuarioCli", backref="cuenta_auth", uselist=False)
+    usuario = relationship("Usuarios", back_populates="cuenta", uselist=False)
